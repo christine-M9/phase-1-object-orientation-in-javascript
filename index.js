1bcf6eab90c3b2a71956da062c32b88bb883819f
Christine <christinekhekama99@gmail.com>
2023-07-12 16:00:02 +0300

    final commit

---
+++ index.js
@@ -0,0 +1,32 @@
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    sayHello() {
+      console.log(`Hello, my name is ${this.name}.`);
+    }
+  
+    sayAge() {
+      console.log(`I am ${this.age} years old.`);
+    }
+  
+    haveBirthday(age) {
+      console.log(`It's my birthday!`);
+      this.age += 1;
+    }
+  }
+  
+  let christine = new Person('Christine', 24);
+  
+  christine.sayHello();
+  // => Hello, my name is Christine.
+  christine.sayAge();
+  // => I am 24 years old.
+  christine.haveBirthday();
+  // => It's my birthday.
+  christine.sayAge();
+  // => I am 24 years old.
+  christine;
+  // => Person { name: 'Christine', age: 24 }
